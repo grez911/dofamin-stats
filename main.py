9ffa6e4f92983fe32956f2ef1662e804a17eb23d
grez911 <grez911@gmail.com>
2018-06-19 17:54:37 +0300

    v 0.4

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # Project page: https://github.com/grez911/dofamin-stats
 
 import requests
-import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 from bs4 import BeautifulSoup
@@ -22,249 +21,7 @@ def get_data():
     page += 1
     print(f"Collecting data from page #{page}...")
     html = requests.get(f"https://dofamin.org/index.php?v=Main&page={page}")
-    html = html.content.decode('UTF-8')
-    soup = BeautifulSoup(html, 'html.parser')
-    divs = soup.find_all('div', class_="nofap-ranks__element "
-                                     + "nofap-ranks__element_period")
-    if len(divs) == 0:
-      break
-    for div in divs:
-      days = int(div.text.split()[0])
-      data.setdefault(days, 0)
-      data[days] += 1
-  return data
-
-def prepare(data):
-  '''
-  Make lists for x and y axis for plotting.
-
-  Input:
-  data - dictionary with days as keys and values as number of people.
-
-  Output:
-  x - numpy array with values for x axis;
-  y - numpy array with values for y axis.
-  '''
-  x = []
-  y = []
-  for days in sorted(data):
-    x.append(days)
-    y.append(data[days])
-  x = np.array(x) + 0.5
-  y = np.array(y)
-  y = y / np.sum(y)
-  return x, y
-
-def mse(y1, y2):
-  '''
-  Mean square error.
-
-  Input:
-  y1 - first numpy array;
-  y2 - second numpy array.
-
-  Output a mean square error.
-  '''
-  return ((y1 - y2) ** 2).mean()
-
-def find_curve(x, y):
-  '''
-  Find distribution function via gradient descend.
-
-  Input:
-  x - numpy array with values for x axis (actual data);
-  y - numpy array with values for y axis (actual data).
-
-  Output:
-  xc - numpy array with values for x axis (fitted curve);
-  yc - numpy array with values for y axis (fitted curve).
-  '''
-  #ax.semilogx(t, 60/t)
-  print("Making gradient descent...")
-  #xc = np.arange(0.5, 500.5, 1)
-  m = len(x) # Number of training points.
-  learning_rate = 0.001
-
-  # a = 1
-  # b = 1
-  # c = 0
-  # for i in range(100000):
-    # yc = a * np.exp(-b * (x - c))
-    # if i % 1000 == 0:
-      # print(f"Step {i}: loss = {mse(yc, y)}")
-
-
-
-
-  Output:
-  xc - numpy array with values for x axis (fitted curve);
-  yc - numpy array with values for y axis (fitted curve).
-  yc - numpy array with values for y axis (fitted curve).
-  '''
-  #ax.semilogx(t, 60/t)
-  print("Making gradient descent...")
-  #xc = np.arange(0.5, 500.5, 1)
-  m = len(x) # Number of training points.
-  learning_rate = 0.001
-
-  # a = 1
-  # b = 1
-  # c = 0
-  # for i in range(100000):
-    # yc = a * np.exp(-b * (x - c))
-    # if i % 1000 == 0:
-      # print(f"Step {i}: loss = {mse(yc, y)}")
-      # print(a,b,c)
-    # da = np.sum(2/m * (yc - y) * (np.exp(-b * (x - c))))
-    # db = np.sum(-2/m * (yc - y) * a * (x - c) * np.exp(-b * (x - c)))
-    # dc = np.sum(2/m * (yc - y) * a * b * np.exp(-b * (x - c)))
-    # a -= da * learning_rate
-    # b -= db * learning_rate
-    # c -= dc * learning_rate
-
-  # a = 0.9
-  # b = 1
-  # c = 1
-  # d = 0.01
-  # for i in range(200000):
-    # yc = a**(b * x + c) + d
-    # if i % 2000 == 0:
-      # print(f"Step {i}: loss = {mse(yc, y)}")
-    # da = np.sum(2/m * (yc - y) * (b * x + c) * a**(b * x + c - 1))
-    # db = np.sum(2/m * (yc - y) * x * np.log(a) * a**(b * x + c))
-    # dc = np.sum(2/m * (yc - y) * np.log(a) * a**(b * x + c))
-    # dd = np.sum(2/m * (yc - y))
-    # a -= da * learning_rate
-    # b -= db * learning_rate
-    # c -= dc * learning_rate
-    # d -= dd * learning_rate
-
-                                     + "nofap-ranks__element_period")
-    if len(divs) == 0:
-      break
-    for div in divs:
-      days = int(div.text.split()[0])
-      data.setdefault(days, 0)
-      data[days] += 1
-  return data
-
-def prepare(data):
-  '''
-  Make lists for x and y axis for plotting.
-
-  Input:
-  data - dictionary with days as keys and values as number of people.
-
-  Output:
-  x - numpy array with values for x axis;
-  y - numpy array with values for y axis.
-  '''
-  x = []
-  y = []
-  # for days in sorted(data):
-    # x.append(days)
-    # y.append(data[days])
-  # x = np.array(x) + 0.5
-  # y = np.array(y)
-  for day in range(0, 500):
-    x.append(days)
-    if day in data:
-      y.append(data[days])
-    else:
-      y.append(0)
-  x = np.array(x) + 0.5
-  y = np.array(y)
-  y = y / np.sum(y)
-  return x, y
-
-def mse(y1, y2):
-  '''
-  Mean square error.
-
-  Input:
-  y1 - first numpy array;
-  y2 - second numpy array.
-
-  Output a mean square error.
-  '''
-  return ((y1 - y2) ** 2).mean()
-
-def find_curve(x, y):
-  '''
-  Find distribution function via gradient descend.
-
-  Input:
-  x - numpy array with values for x axis (actual data);
-  y - numpy array with values for y axis (actual data).
-
-  Output:
-  xc - numpy array with values for x axis (fitted curve);
-  yc - numpy array with values for y axis (fitted curve).
-  '''
-  #ax.semilogx(t, 60/t)
-  print("Making gradient descent...")
-  #xc = np.arange(0.5, 500.5, 1)
-  m = len(x) # Number of training points.
-  learning_rate = 0.001
-
-  # a = 1
-  # b = 1
-  # c = 0
-  # for i in range(100000):
-    # yc = a * np.exp(-b * (x - c))
-    # if i % 1000 == 0:
-      # print(f"Step {i}: loss = {mse(yc, y)}")
-      # print(a,b,c)
-    # da = np.sum(2/m * (yc - y) * (np.exp(-b * (x - c))))
-    # db = np.sum(-2/m * (yc - y) * a * (x - c) * np.exp(-b * (x - c)))
-    # dc = np.sum(2/m * (yc - y) * a * b * np.exp(-b * (x - c)))
-    # a -= da * learning_rate
-    # b -= db * learning_rate
-    # c -= dc * learning_rate
-
-  # a = 0.9
-  # b = 1
-  # c = 1
-  # d = 0.01
-  # for i in range(200000):
-    # yc = a**(b * x + c) + d
-  data - dictionary with days as keys and values as number of people.
-      break
-      break
-      break
-    for div in divs:
-      days = int(div.text.split()[0])
-      data.setdefault(days, 0)
-      data[days] += 1
-  return data
-
-def prepare(data):
-  '''
-#!/usr/bin/env python3
-# Project page: https://github.com/grez911/dofamin-stats
-
-import requests
-import pickle
-import matplotlib.pyplot as plt
-import numpy as np
-from bs4 import BeautifulSoup
-
-def get_data():
-  '''
-  Get data from the site.
-
-  Inputs nothing.
-  
-  Output:
-  data - dictionary with days as keys and values as number of people.
-  '''
-  page = 0
-  data = dict()
-  while True:
-    page += 1
-    print(f"Collecting data from page #{page}...")
-    html = requests.get(f"https://dofamin.org/index.php?v=Main&page={page}")
-    html = html.content.decode('UTF-8')
+    html = html.content.decode('UTF-8', errors='ignore')
     soup = BeautifulSoup(html, 'html.parser')
     divs = soup.find_all('div', class_="nofap-ranks__element "
                                      + "nofap-ranks__element_period")
@@ -295,8 +52,9 @@ def prepare(data):
       y.append(data[day])
     else:
       y.append(0)
-  # Add half a day because in the table on site displayed floor values.
-  # So such result as "2 days" actually between 2 and 3 days.
+  # Add half a day because on the sitedisplayed floor values,
+  # e.g. "2 days" means time between 2 and 3 days, so we are
+  # taking an average.
   x = np.array(x) + 0.5
   y = np.array(y)
   y = y / np.sum(y)
@@ -314,85 +72,68 @@ def mse(y1, y2):
   '''
   return ((y1 - y2) ** 2).mean()
 
-def find_curve(x, y):
+def find_curve(x, y, learning_rate, epochs):
   '''
   Find distribution function via gradient descend.
 
   Input:
   x - numpy array with values for x axis (actual data);
-  y - numpy array with values for y axis (actual data).
+  y - numpy array with values for y axis (actual data);
+  learning_rate - learning rate;
+  epochs - number of epochs (we are doing a full batch training).
 
   Output:
-  xc - numpy array with values for x axis (fitted curve);
+  xc - numpy array with values for x axis (fitted curve).
   yc - numpy array with values for y axis (fitted curve).
   '''
-  #ax.semilogx(t, 60/t)
   print("Making gradient descent...")
-  #xc = np.arange(0.5, 500.5, 1)
   m = len(x) # Number of training points.
-  learning_rate = 0.001
-
-  # a = 1
-  # b = 1
-  # c = 0
-  # for i in range(100000):
-    # yc = a * np.exp(-b * (x - c))
-    # if i % 1000 == 0:
-      # print(f"Step {i}: loss = {mse(yc, y)}")
-      # print(a,b,c)
-    # da = np.sum(2/m * (yc - y) * (np.exp(-b * (x - c))))
-    # db = np.sum(-2/m * (yc - y) * a * (x - c) * np.exp(-b * (x - c)))
-    # dc = np.sum(2/m * (yc - y) * a * b * np.exp(-b * (x - c)))
-    # a -= da * learning_rate
-    # b -= db * learning_rate
-    # c -= dc * learning_rate
-
-  # a = 0.9
-  # b = 1
-  # c = 1
-  # d = 0.01
-  # for i in range(200000):
-    # yc = a**(b * x + c) + d
-    # if i % 2000 == 0:
-      # print(f"Step {i}: loss = {mse(yc, y)}")
-    # da = np.sum(2/m * (yc - y) * (b * x + c) * a**(b * x + c - 1))
-    # db = np.sum(2/m * (yc - y) * x * np.log(a) * a**(b * x + c))
-    # dc = np.sum(2/m * (yc - y) * np.log(a) * a**(b * x + c))
-
-      # print(a,b,c)
-    # da = np.sum(2/m * (yc - y) * (np.exp(-b * (x - c))))
-    # db = np.sum(-2/m * (yc - y) * a * (x - c) * np.exp(-b * (x - c))
-    # dc = np.sum(2/m * (yc - y) * a * b * np.exp(-b * (x - c)))
-    # a -= da * learning_rate
-    # b -= db * learning_rate
-    # c -= dc * learning_rate
-
-  a = 0.9
-  b = 1
-  c = 1
-  d = 0.01
-  yc = a**(b * x + c) + d
-  return x, yc
+  a = 0.27
+  b = -0.35
+  c = 0
+  d = 0
+  for i in range(epochs+1):
+    if i % 1000 == 0:
+      yc = a * np.exp(b * x + c) + d
+      loss = mse(y, yc)
+      print(f"Epoch {i}: loss = {loss}")
+      print(f"a = {a}, b = {b}")
+    da = np.sum(2/m * (yc - y) * (np.exp(b * x + c)))
+    db = np.sum(2/m * (yc - y) * a * x * np.exp(b * x + c))
+    dc = np.sum(2/m * (yc - y) * a * b * np.exp(b * (x + c)))
+    dd = np.sum(2/m * (yc - y))
+    a -= da * learning_rate
+    b -= db * learning_rate
+#    c -= dc * learning_rate
+#    d -= dd * learning_rate
+  a = np.around(a, 3)
+  b = np.around(b, 3)
+  print("-------")
+  print("Found solution for the function y(x) = a * e^(b * x + c) + d:")
+  print(f"a = {a}")
+  print(f"b = {b}")
+  print(f"c = {c}")
+  print(f"d = {d}")
+  xc = np.logspace(np.log10(0.5), np.log10(max(x)))
+  yc = a * np.exp(b * xc + c) + d
+  return xc, yc, (a, b, c, d)
 
 def plot(x, y):
   '''
-  Create plot.png.
-
-  Input:
-  x - numpy array with values for x axis;
-  y - numpy array with values for y axis.
+  Draw plot.
 
   Outputs nothing.
   '''
   fig, ax = plt.subplots()
   ax.semilogx(x, y, 'b.')
-  xc, yc = find_curve(x, y)
-  ax.semilogx(xc, yc)
-  ax.set(xlabel='nofap time (days)', ylabel='proportion of people',
-         title='Nofap time distribution')
-  #ax.grid(True, which='both')
+  xc, yc, params = find_curve(x, y, learning_rate=5 * 10**(-3), epochs=2 * 10**5)
+  #xc, yc, params = find_curve(x, y, learning_rate=10**(-3), epochs= 10**3)
+  ax.semilogx(xc, yc, '#ff7f0e', label=f'${params[0]}e^{{{params[1]}x}}$')
+  plt.title("Nofap time distribution")
+  plt.xlabel("nofap time (days)")
+  plt.ylabel("proportion of people")
   ax.grid()
-  fig.savefig("plot.png")
+  plt.legend()
   plt.show()
 
 def main():
@@ -400,14 +141,12 @@ def main():
   Main function.
   Inputs and outputs nothing.
   '''
-  #pickle.dump(prepare(get_data()), open("data.p", 'wb'))
-  x, y = pickle.load(open("data.p", 'rb'))
+  #import pickle
+  data = get_data()
+  #pickle.dump(data, open("data.p", 'wb'))
+  #x, y = prepare(pickle.load(open("data.p", 'rb')))
+  x, y = prepare(data)
   plot(x, y)
-  #total = np.sum(y)
-  #avg = np.sum(x * y) / total
-  #print("-----")
-  #print(f"Total number of people: {total}")
-  #print(f"Average nofap time: {avg:.1f} days")
 
 if __name__ == '__main__':
   main()
