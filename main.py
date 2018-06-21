3d487b9ee47b26e02da4a5d8f183756dd56ee44e
grez911 <grez911@gmail.com>
2018-06-21 12:27:04 +0300

    MSE + exponential lambda

--- main.py
+++ main.py
@@ -88,35 +88,43 @@ def find_curve(x, y, learning_rate, epochs):
   '''
   print("Making gradient descent...")
   m = len(x) # Number of training points.
-  a = 0.27
-  b = -0.35
-  c = 0
-  d = 0
+  a = 0.206
+  b = -0.258
+  c = 0.0583
+  d = 0.0922
+  h = 0
+  k = 0
   for i in range(epochs+1):
     if i % 1000 == 0:
-      yc = a * np.exp(b * x + c) + d
+      yc = a * np.exp((b * np.exp(c * x) + d) * x + h) + k
       loss = mse(y, yc)
       print(f"Epoch {i}: loss = {loss}")
-      print(f"a = {a}, b = {b}")
-    da = np.sum(2/m * (yc - y) * (np.exp(b * x + c)))
-    db = np.sum(2/m * (yc - y) * a * x * np.exp(b * x + c))
-    dc = np.sum(2/m * (yc - y) * a * b * np.exp(b * (x + c)))
-    dd = np.sum(2/m * (yc - y))
+      print(f"a = {a}, b = {b}, c = {c}, d = {d}, h = {h}, k = {k}")
+    da = np.sum(2/m * (yc - y) * np.exp(x * (b * np.exp(c * x) + d) +h))
+    db = np.sum(2/m * (yc - y) * a * x * np.exp(x * (b * np.exp(c * x) + d) + c * x + h))
+    dc = np.sum(2/m * (yc - y) * a * b * x**2 * np.exp(x * (b * np.exp(c * x) + d) + c * x + h))
+    dd = np.sum(2/m * (yc - y) * a * x * np.exp(x * (b * np.exp(c * x) + d) + h))
+    dh = np.sum(2/m * (yc - y) * a * np.exp(x * (b * np.exp(c * x) + d) + h))
+    dk = np.sum(2/m * (yc - y))
     a -= da * learning_rate
     b -= db * learning_rate
-#    c -= dc * learning_rate
-#    d -= dd * learning_rate
-  a = np.around(a, 3)
-  b = np.around(b, 3)
+    c -= dc * learning_rate
+    d -= dd * learning_rate
+#    h -= dh * learning_rate
+#    k -= dk * learning_rate
+  #a = np.around(a, 3)
+  #b = np.around(b, 3)
   print("-------")
-  print("Found solution for the function y(x) = a * e^(b * x + c) + d:")
+  print("Found solution for the function y(x) = a * e^((b * e^(c * x) + d) * x + h) + k:")
   print(f"a = {a}")
   print(f"b = {b}")
   print(f"c = {c}")
   print(f"d = {d}")
+  print(f"h = {h}")
+  print(f"k = {k}")
   xc = np.logspace(np.log10(0.5), np.log10(max(x)))
-  yc = a * np.exp(b * xc + c) + d
-  return xc, yc, (a, b, c, d)
+  yc = a * np.exp((b * np.exp(c * xc) + d) * xc + h) + k
+  return xc, yc, (a, b, c, d, h, k)
 
 def plot(x, y):
   '''
@@ -126,8 +134,8 @@ def plot(x, y):
   '''
   fig, ax = plt.subplots()
   ax.semilogx(x, y, 'b.')
-  xc, yc, params = find_curve(x, y, learning_rate=5 * 10**(-3), epochs=2 * 10**5)
-  ax.semilogx(xc, yc, '#ff7f0e', label=f'${params[0]}e^{{{params[1]}x}}$')
+  xc, yc, params = find_curve(x, y, learning_rate=100 * 10**(-3), epochs=1 * 10**5)
+  ax.semilogx(xc, yc, '#ff7f0e')#, label=f'${params[0]}e^{{{params[1]}x}}$')
   plt.title("Nofap time distribution")
   plt.xlabel("nofap time (days)")
   plt.ylabel("proportion of people")
@@ -141,10 +149,10 @@ def main():
   Inputs and outputs nothing.
   '''
   plt.rc('font', size=16)
-  data = get_data()
+#  data = get_data()
 
   import pickle
-  pickle.dump(data, open("data.p", "wb"))
+#  pickle.dump(data, open("data.p", "wb"))
   data = pickle.load(open("data.p", "rb" ))
   x, y = prepare(data)
   plot(x, y)
