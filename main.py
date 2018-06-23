1c39329122fef4dc12595237702a4697f54d5d47
grez911 <grez911@gmail.com>
2018-06-23 18:01:46 +0300

    v 1.4

--- main.py
+++ main.py
@@ -58,6 +58,9 @@ def prepare(data):
   x = np.array(x) + 0.5
   y = np.array(y)
   y = y / np.sum(y)
+
+  y = fill_gaps(y)
+  y = np.log(y)
   return x, y
 
 def fill_gaps(y):
@@ -119,7 +122,7 @@ def dmae(y_pred, y_true):
   '''
   return (y_pred > y_true) * 2 - 1
 
-def find_curve(x, y, learning_rate, epochs):
+def find_curve(x, y, learning_rate):
   '''
   Find distribution function via gradient descend.
 
@@ -127,7 +130,6 @@ def find_curve(x, y, learning_rate, epochs):
   x - numpy array with values for x axis (actual data);
   y - numpy array with values for y axis (actual data);
   learning_rate - learning rate;
-  epochs - number of epochs (we are doing a full batch training).
 
   Output:
   xc - numpy array with values for x axis (fitted curve).
@@ -135,65 +137,87 @@ def find_curve(x, y, learning_rate, epochs):
   '''
   print("Making gradient descent...")
   m = len(x) # Number of training points.
-  a = 0.31379330453162924
-  b = 0.28748657127515836
-  c = -0.05986857836271425
-  d = 0.1357512985372947
-  h = -0.2899616489649545
-  k = 0.029834249848520063
-  p = -8.789158028292139
 
-  # a = 1.7903939776338165
-  # b = 0
-  # c = 0
-  # d = 0
-  # h = -2.755929132216683
-  # k = 1.5833140484509383
-  # p = -8.911237101387389
-  for i in range(epochs+1):
-    if i % 1000 == 0:
-      yc = a * np.exp((b * np.exp(c * x + d) + h) * x + k) + p
-      #loss = mae(yc, y)
-      loss = mse(yc, y)
-      print(f"Epoch {i}: loss = {loss}")
-      print(f"a = {a}, b = {b}, c = {c}, d = {d}, h = {h}, k = {k}, p = {p}")
-    da = np.sum(2/m * (yc - y) * np.exp(x * (b * np.exp(c * x + d) + h) + k))
-    db = np.sum(2/m * (yc - y) * a * x * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
-    dc = np.sum(2/m * (yc - y) * a * b * x**2 * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
-    dd = np.sum(2/m * (yc - y) * a * b * x * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
-    dh = np.sum(2/m * (yc - y) * a * x * np.exp(x * (b * np.exp(c * x + d) + h) + k))
-    dk = np.sum(2/m * (yc - y) * a * np.exp(x * (b * np.exp(c * x + d) + h) + k))
-    dp = np.sum(2/m * (yc - y))
+  p = np.random.randn(7) * 0.1
+  p[6] = -10
+  dp = np.zeros(7)
+  eps = 10**(-8)
+  M = np.zeros(7)
+  R = np.zeros(7)
+  beta1 = 0.9
+  beta2 = 0.999
 
-    # da = np.sum(2/m * dmae(yc, y) * np.exp(x * (b * np.exp(c * x + d) + h) + k))
-    # #db = np.sum(2/m * dmae(yc, y) * a * x * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
-    # #dc = np.sum(2/m * dmae(yc, y) * a * b * x**2 * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
-    # #dd = np.sum(2/m * dmae(yc, y) * a * b * x * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
-    # dh = np.sum(2/m * dmae(yc, y) * a * x * np.exp(x * (b * np.exp(c * x + d) + h) + k))
-    # dk = np.sum(2/m * dmae(yc, y) * a * np.exp(x * (b * np.exp(c * x + d) + h) + k))
-    # #dp = np.sum(2/m * dmae(yc, y))
+  # a = 3.279210148249056
+  # b = -0.006483609824476167
+  # c = 0.5965040704470368
+  # d = -10.612307583296714
+  i = 0
+  try:
+    while True:
+      # yc = a * np.exp(b * x + c) + d
+      yc = p[0] * np.exp((p[1] * np.exp(p[2] * x + p[3]) + p[4]) * x + p[5]) + p[6]
+      if i % 1000 == 0:
+        #loss = mae(yc, y)
+        loss = mse(yc, y)
+        print(f"Epoch {i}: loss = {loss}")
+        # print(f"a = {a}, b = {b}, c = {c}, d = {d}")
+        print(p[0], p[1], p[2], p[3], p[4], p[5], p[6])
+  
+      # da = np.sum(2/m * (yc - y) * (np.exp(b * x + c)))
+      # db = np.sum(2/m * (yc - y) * a * x * np.exp(b * x + c))
+      # dc = np.sum(2/m * (yc - y) * a * b * np.exp(b * (x + c)))
+      # dd = np.sum(2/m * (yc - y))
+  
+      dp[0] = np.sum(2/m * (yc - y) * np.exp(x * (p[1] * np.exp(p[2] * x + p[3]) + p[4]) + p[5]))
+      dp[1] = np.sum(2/m * (yc - y) * p[0] * x * np.exp(x * (p[1] * np.exp(p[2] * x + p[3]) + p[4]) + p[2] * x + p[3] + p[5]))
+      dp[2] = np.sum(2/m * (yc - y) * p[0] * p[1] * x**2 * np.exp(x * (p[1] * np.exp(p[2] * x + p[3]) + p[4]) + p[2] * x + p[3] + p[5]))
+      dp[3] = np.sum(2/m * (yc - y) * p[0] * p[1] * x * np.exp(x * (p[1] * np.exp(p[2] * x + p[3]) + p[4]) + p[2] * x + p[3] + p[5]))
+      dp[4] = np.sum(2/m * (yc - y) * p[0] * x * np.exp(x * (p[1] * np.exp(p[2] * x + p[3]) + p[4]) + p[5]))
+      dp[5] = np.sum(2/m * (yc - y) * p[0] * np.exp(x * (p[1] * np.exp(p[2] * x + p[3]) + p[4]) + p[5]))
+      dp[6] = np.sum(2/m * (yc - y))
+  
+      # da = np.sum(2/m * dmae(yc, y) * np.exp(x * (b * np.exp(c * x + d) + h) + k))
+      # #db = np.sum(2/m * dmae(yc, y) * a * x * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
+      # #dc = np.sum(2/m * dmae(yc, y) * a * b * x**2 * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
+      # #dd = np.sum(2/m * dmae(yc, y) * a * b * x * np.exp(x * (b * np.exp(c * x + d) + h) + c * x + d + k))
+      # dh = np.sum(2/m * dmae(yc, y) * a * x * np.exp(x * (b * np.exp(c * x + d) + h) + k))
+      # dk = np.sum(2/m * dmae(yc, y) * a * np.exp(x * (b * np.exp(c * x + d) + h) + k))
+      # #dp = np.sum(2/m * dmae(yc, y))
     
-    a -= da * learning_rate
-    b -= db * learning_rate
-    c -= dc * learning_rate
-    d -= dd * learning_rate
-    h -= dh * learning_rate
-    k -= dk * learning_rate
-    p -= dp * learning_rate
+      M = beta1 * M + (1.0 - beta1) * dp
+      R = beta2 * R + (1.0 - beta2) * dp**2
+      m_hat = M / (1.0 - beta1**(i+1))
+      r_hat = R / (1.0 - beta2**(i+1))
+      p -= learning_rate * m_hat / (np.sqrt(r_hat) + eps)
+      i += 1
+  except KeyboardInterrupt:
+
+
+    # a -= da * learning_rate * 100000
+    # b -= db * learning_rate * 100000
+    # c -= dc * learning_rate * 10000
+    # d -= dd * learning_rate * 100000
+    # h -= dh * learning_rate
+    # k -= dk * learning_rate * 1000
+    # p -= dp * learning_rate * 1000
+
   #a = np.around(a, 3)
   #b = np.around(b, 3)
-  print("-------")
-  print("Found solution for the function y(x) = a * e^((b * e^(c * x + d) + h) * x + k) + p:")
-  print(f"a = {a}")
-  print(f"b = {b}")
-  print(f"c = {c}")
-  print(f"d = {d}")
-  print(f"h = {h}")
-  print(f"k = {k}")
-  print(f"p = {p}")
-  xc = np.logspace(np.log10(0.5), np.log10(max(x)))
-  yc = a * np.exp((b * np.exp(c * xc + d) + h) * xc + k) + p
-  return xc, yc, (a, b, c, d, h, k, p)
+    print("-------")
+    # print("Found solution for the function y(x) = a * np.exp(b * x + c) + d:")
+    #print("Found solution for the function y(x) = a * e^((b * e^(c * x + d) + h) * x + k) + p:")
+    print(f"p[0] = {p[0]}")
+    print(f"p[1] = {p[1]}")
+    print(f"p[2] = {p[2]}")
+    print(f"p[3] = {p[3]}")
+    print(f"p[4] = {p[4]}")
+    print(f"p[5] = {p[5]}")
+    print(f"p[6] = {p[6]}")
+    xc = np.logspace(np.log10(0.5), np.log10(max(x)))
+    #yc = a * np.exp(b * xc + c) + d
+    yc = p[0] * np.exp((p[1] * np.exp(p[2] * xc + p[3]) + p[4]) * xc + p[5]) + p[6]
+    #return xc, yc, (a, b, c, d)
+    return xc, yc, p
 
 def plot(x, y):
   '''
@@ -202,11 +226,9 @@ def plot(x, y):
   Outputs nothing.
   '''
   fig, ax = plt.subplots()
-  y = fill_gaps(y)
-  y = np.log(y)
   #ax.plot(x, y, 'b.')
   ax.semilogx(x, y, 'b.')
-  xc, yc, params = find_curve(x, y, learning_rate=1 * 10**(-5), epochs=10 * 10**5)
+  xc, yc, params = find_curve(x, y, learning_rate=1 * 10**(-3))
   ax.semilogx(xc, yc, '#ff7f0e')#, label=f'${params[0]}e^{{{params[1]}x}}$')
   #ax.plot(xc, yc, '#ff7f0e')
   plt.title("Nofap time distribution")
